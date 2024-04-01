9b0f42fe0dfe7d25c9fa45a97c5cbf40ce248080
Julius Kibbassa <148614048+Julius-Kibbs@users.noreply.github.com>
2024-04-01 23:10:17 +0300

    Add files via upload

---
+++ assignment.dart
@@ -0,0 +1,26 @@
+void main(){
+ //data types
+
+ //strings
+ String name = "Julius";
+ print("My name is $name");
+
+ //integers and floats
+ int age = 22;
+ print("I am $age years old");
+ double gpa = 3.4;
+ print("I have a GPA of $gpa");
+
+ //lists
+ List<String> food = ['burger', 'pizza', 'hotdogs'];
+ print("I love to eat $food");
+
+
+ //maps
+ Map<String, int> friends = {
+  'Jacob' : 22,
+  'Samuel' : 25,
+  'Salha' : 20,
+ };
+print("Here are my friends and their ages $friends");
+}
