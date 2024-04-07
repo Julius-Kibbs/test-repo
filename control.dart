845babfd6be2aa6f8633ec74608d58ea8b6c1729
Julius Kibbassa <148614048+Julius-Kibbs@users.noreply.github.com>
2024-04-07 12:39:48 +0300

    Add files via upload

---
+++ control.dart
@@ -0,0 +1,14 @@
+void main(){
+  int number = 10;
+  if(number > 10){
+    print("The number is greater than 10");
+  }
+
+  else if(number < 10){
+    print("The number is less than 10");
+  }
+
+  else{
+    print("The number is equal to 10");
+  }
+}
